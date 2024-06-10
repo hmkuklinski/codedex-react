ce3064708b6b0ca89e1603455cdbac82d1ac4beb
hmkuklinski <hmkuklinski@gmail.com>
2024-06-10 04:04:08 -0400

    added lesson6
    added styling to base code

---
+++ lesson6/App.js
@@ -0,0 +1,47 @@
+export default function App() {
+  return (
+    <div className="container"> 
+      <h1>Tour Stops</h1>
+      
+      <ul>
+        <div className="entry">
+          <div className="entry-img">
+          <img src="https://th.bing.com/th/id/R.3b9af08861bf1bf301e96454b4842423?rik=nZk3iT61HTtKQQ&pid=ImgRaw&r=0" alt="madison square garden"></img>
+            </div>
+          <li>🇺🇸 New York City, US - Madison Square Garden</li>
+        </div>
+
+        
+        <div className="entry">
+           <div className="entry-img">
+          <img src="https://www.reisroutes.be/userfiles/fotos/nieuwe-wembley-stadium_2079_xl.jpg" alt="london"></img>
+          </div>
+          <li>🇬🇧 London, UK - Wembley Stadium</li>
+          </div>
+
+        
+        <div className="entry">
+           <div className="entry-img">
+          <img src="https://thumbs.dreamstime.com/b/stadium-zenith-arena-night-illuminated-multi-colored-lights-116825993.jpg" alt="munich"></img>
+          </div>
+          <li>🇩🇪 Munich, DE - Zenith Halle</li>
+          </div>
+
+        
+        <div className="entry">
+           <div className="entry-img">
+          <img src="https://th.bing.com/th/id/OIP._XSLffg7PC8cVtOC7qTY4wAAAA?rs=1&pid=ImgDetMain" alt="tokyo"></img>
+            </div>
+          <li>🇯🇵 Tokyo, JP - Budokan</li>
+        </div>
+        
+        <div className="entry">
+           <div className="entry-img">
+          <img src="https://media.timeout.com/images/104096395/630/472/image.jpg" alt="australia"></img>
+          </div>
+        <li>🇦🇺 Melbourne, AU - The Forum</li>
+          </div>
+      </ul>
+    </div>
+  );
+}
