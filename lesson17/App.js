26126094a0af7fa956cc3c7e97ea84c42936182b
hmkuklinski <hmkuklinski@gmail.com>
2024-06-22 23:27:27 -0400

    hover events

---
+++ lesson17/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function App(props) {
+  // State to track tooltip visibility
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  // define handleMouseEnter() here 💖
+  function handleMouseEnter(){
+    setShowTooltip(true);
+  }
+
+  // define handleMouseLeave() here 💖
+  function handleMouseLeave(){
+    setShowTooltip(false);
+  }
+
+  return (
+    <div className="App">
+      <div
+        className="element-with-tooltip"
+        onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+      >
+        Hover over me
+        {showTooltip && <div className="tooltip">This is a tooltip</div>}
+      </div>
+    </div>
+  );
+}
